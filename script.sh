525888d94836c9736257e2e1b8875f2af745c938
deepika-ponrangam <deepika.ponrangam@batonsystems.com>
2024-02-06 15:03:41 +0530

    Modified grep

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ read -p "Enter the link to the book : " link
 
 echo -e "\n"
 
-wget $link -O - | tr '[:punct:]' ' ' | tr 'A-Z' 'a-z' | tr ' ' '\n' | grep -vwFf stopwords.txt | sort | uniq -c | sort -rn | head -10
+wget $link -O - | grep -oE "[[:alpha:]]+" | grep -ivwFf stopwords.txt | sort | uniq -c | sort -rn | head -10
