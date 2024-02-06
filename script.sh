d56b95dcf1cb2daae029283bfac7468c89bdfbfc
deepika-ponrangam <deepika.ponrangam@batonsystems.com>
2024-02-06 12:49:58 +0530

    Print frequent words excluding stopwords

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ read -sp "Enter the link to the book : " link
 
 echo -e "\nYour link is $link\n"
 
-wget $link -O - | tr '[:punct:]' ' ' | tr 'A-Z' 'a-z' | tr -s ' ' | tr ' ' '\n' | sort | uniq -c | sort -rn | head -10
+wget $link -O - | tr '[:punct:]' ' ' | tr 'A-Z' 'a-z' | tr ' ' '\n' | grep -vwFf stopwords.txt | sort | uniq -c | sort -rn | head -10
